c744aacd506ab1e917a6fda68f277593389deb8d
cyrinefakhfakh <sirinefakhfakh03@gmail.com>
2024-03-24 20:21:26 +0100

    first commit

---
+++ admin/register.php
@@ -0,0 +1,114 @@
+<?php include '../components/connect.php';
+if (isset($_POST['submit'])) {
+    $id = unique_id();
+    $name = $_POST['name'];
+    $name =filter_var($name, FILTER_SANITIZE_STRING);
+
+    $email = $_POST['email'];
+    $email=filter_var($email, FILTER_SANITIZE_EMAIL);
+
+    $pass = $_POST['pass'];
+    $pass =filter_var($pass, FILTER_SANITIZE_STRING);
+
+    $cpass = $_POST['cpass'];
+    $cpass =filter_var($cpass, FILTER_SANITIZE_STRING);
+
+    $image = $_FILES['image']['name'];
+    $image =filter_var($image, FILTER_SANITIZE_STRING);
+    $ext = pathinfo($image, PATHINFO_EXTENSION);
+
+    $rename = unique_id() . "." . $ext;
+    $image_size=$_FILES['image']['size'];
+    $image_tmp_name = $_FILES['image']['tmp_name'];
+    $image_folder = "../images/";
+    $select_seller=$conn->prepare("SELECT * FROM sellers WHERE email=?");
+    $select_seller->execute([$email]);
+    if($select_seller->rowCount()==0){
+        if ($pass == $cpass) {
+            if ($ext == 'jpg' || $ext == 'png' || $ext == 'jpeg') {
+                if ($image_size < 1000000) {
+                    $insert_seller = $conn->prepare("INSERT INTO sellers(name,email,password,image) VALUES(?,?,?,?)");
+                    $insert_seller->execute([$name, $email, $pass, $rename]);
+                    move_uploaded_file($image_tmp_name, $image_folder . $rename);
+                    $success_msg[] = "Seller registered successfully";
+                } else {
+                    $error_msg[] = "Image size is too large";
+                }
+            } else {
+                $error_msg[] = "Image format is not supported";
+            }
+        } else {
+            $error_msg[] = "Password does not match";
+        }
+    }
+    else{
+        $error_msg[]="Email already exists";
+    }
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html >
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Blue Sky Summer registration</title>
+    <link rel="stylesheet" type="text/css" href="../css/admin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+<body>
+    <div class="form-container">
+       <form action="" method="post" enctype="multipart/form-data" class="register">
+            <h3>register now</h3>
+            <div class="flex">
+                <div class="col">
+                    <div class="input-field">
+                        <p>your name<span>*</span></p>
+                        <input type="text" name="name" placeholder="enter your name" required class="box">
+                    </div>
+                    <div class="input-field">
+                        <p>your email<span>*</span></p>
+                        <input type="email" name="email" placeholder="enter your email" required class="box">
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="input-field">
+                        <p>your password<span>*</span></p>
+                        <input type="password" name="pass" placeholder="enter your password" required class="box">
+                    </div>
+                    <div class="input-field">
+                        <p>confirm password<span>*</span></p>
+                        <input type="password" name="cpass" placeholder="confirm password" required class="box">
+                    </div>
+                </div>
+                </div>
+                    <div class="input-field">
+                        <p>your profile<span>*</span></p>
+                        <input type="file" name="image" accept="image/*" required class="box">
+                    </div>
+                    <p class="link">already have an account? <a href="login.php">login</a></p>
+                    <input type="submit" name="submit" class="btn" value="register now">
+                
+            </form>
+
+            </div>
+                
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+<script src="../js/script.js"></script>
+<?php include '../components/alert.php'; ?>
+</body>
+</html>
---
+++ components/connect.php
@@ -0,0 +1,22 @@
+<?php
+$db_name='mysql:host=localhost;dbname=icecream_db';
+$user_name='root';
+$password='';
+$conn=new PDO($db_name,$user_name,$password);
+if($conn){
+    echo "Connected";
+}
+else{
+    echo "Not Connected";
+}
+function unique_id(){
+    $chars="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $charLength=strlen($chars);
+    $randomString="";
+    for($i=0;$i<20;$i++){
+        $randomString.=$chars[rand(0,$charLength-1)];
+
+    }
+    return $randomString;
+}   
+?>
